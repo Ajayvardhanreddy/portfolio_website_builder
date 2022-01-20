bfa43cf62043fd3bd5d1abb664b876a352898a54
Ajayvardhanreddy <vardhanreddyajaya@gmail.com>
2022-01-20 16:25:21 +0530

    Update

--- portfolio_builder/app.py
+++ portfolio_builder/app.py
@@ -10,9 +10,7 @@ csrf = CSRFProtect(app)
 
 app.secret_key = ''.join(secrets.choice(string.ascii_uppercase + string.digits)for i in range(28))
 
-# app.config['MONGO_URI'] = os.environ["DATABASE_URI"]
-app.config['MONGO_URI'] = "mongodb+srv://ajayvardhanreddy:ajayvardhan@learnmongo.dba5m.mongodb.net/portfolio?retryWrites=true&w=majority"
-
+app.config['MONGO_URI'] = os.environ["DATABASE_URI"]
 
 mongo = PyMongo(app)
 users_accounts_col = mongo.db.portfolioaccounts
